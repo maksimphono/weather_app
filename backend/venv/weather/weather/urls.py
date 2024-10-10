a965326a317dded3c586ca78b9626f028aee75ec
Maxun <trukhinmaksimof@mail.ru>
2024-10-10 14:22:40 +0700

    created simple initial model, serializer and superuser

--- backend/venv/weather/weather/urls.py
+++ backend/venv/weather/weather/urls.py
@@ -21,5 +21,5 @@ from api import urls as api_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include(api_urls)),
+    #path("api/", include(api_urls))
 ]
