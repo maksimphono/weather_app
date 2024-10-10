d9dd2af7c50e75712a1181ef8442e86ada22d9ee
Maxun <trukhinmaksimof@mail.ru>
2024-10-10 23:21:34 +0700

    configured src folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import Home from "./components/Home.jsx"
 
-ReactDOM.render(<h1>Qwerty</h1>, document.getElementById("root"))
+ReactDOM.render(<></>, document.getElementById("root"))
