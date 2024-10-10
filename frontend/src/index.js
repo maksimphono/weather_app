cd7c3765a9bf0a0667f061511c176a201962d79a
Maxun <trukhinmaksimof@mail.ru>
2024-10-10 23:19:57 +0700

    configured src folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,1 +1,5 @@
-console.log("qwerty")
+import React from "react";
+import ReactDOM from "react-dom";
+//import Home from "./components/Home.jsx"
+
+ReactDOM.render(<h1>Qwerty</h1>, document.getElementById("root"))
