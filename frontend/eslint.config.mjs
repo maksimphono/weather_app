4bdb3942c2a8220a41446c54fbc0f99174fcbdcd
Maxun <trukhinmaksimof@mail.ru>
2024-12-05 22:58:17 +0700

    changed eslint configuration for React

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,11 +2,32 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: {
+      pluginReact,
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      // ... any rules you want
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+    },
+    // ... others are omitted for brevity
+  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
 ];
